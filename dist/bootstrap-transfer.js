18b0b7e0ab85db178c81831b8a93c8d0334f6bfd
Ednei Leite da Silva <edsiddos@gmail.com>
2016-02-01 21:39:56 -0300

    Atualização método de para setar valor do select target, adição da funcionalidade de mover opção ao dar duplo click

--- dist/bootstrap-transfer.js
+++ dist/bootstrap-transfer.js
@@ -1,95 +1,123 @@
+
 (function ($) {
     $.fn.bootstrapTransfer = function (options) {
         var settings = $.extend({}, $.fn.bootstrapTransfer.defaults, options);
-        var _this;
+        var $this;
 
         /* #=============================================================================== */
         /* # Expose public functions */
         /* #=============================================================================== */
 
         this.populate = function (input) {
-            _this.populate(input);
+            $this.populate(input);
         };
 
         this.set_values = function (values) {
-            _this.set_values(values);
+            $this.set_values(values);
         };
 
         this.get_values = function () {
-            return _this.get_values();
+            return $this.get_values();
         };
 
         return this.each(function () {
-            _this = $(this);
+            $this = $(this);
             /* #=============================================================================== */
             /* # Add widget markup */
             /* #=============================================================================== */
-            _this.append($.fn.bootstrapTransfer.defaults.template);
-            _this.addClass("bootstrap-transfer-container");
+            $this.append($.fn.bootstrapTransfer.defaults.template);
+            $this.addClass("bootstrap-transfer-container");
+
             /* #=============================================================================== */
             /* # Initialize internal variables */
             /* #=============================================================================== */
-            _this.$filter_input = _this.find('.filter-input');
-            _this.$remaining_select = _this.find('select.remaining');
-            _this.$target_select = _this.find('select.target');
-            _this.$add_btn = _this.find('.selector-add');
-            _this.$remove_btn = _this.find('.selector-remove');
-            _this.$choose_all_btn = _this.find('.selector-chooseall');
-            _this.$clear_all_btn = _this.find('.selector-clearall');
-            _this._remaining_list = [];
-            _this._target_list = [];
+            $this.$filter_input = $this.find('.filter-input');
+            $this.$remaining_select = $this.find('select.remaining');
+            $this.$target_select = $this.find('select.target');
+            $this.$add_btn = $this.find('.button-add');
+            $this.$remove_btn = $this.find('.button-remove');
+            $this.$choose_all_btn = $this.find('.button-chooseall');
+            $this.$clear_all_btn = $this.find('.button-clearall');
+
+            $this._remaining_list = [];
+            $this._target_list = [];
+
             /* #=============================================================================== */
             /* # Apply settings */
             /* #=============================================================================== */
-            /* target_id */
-            if (settings.target_id != '')
-                _this.$target_select.attr('id', settings.target_id);
-            /* height */
-            _this.find('select.filtered').css('height', settings.height);
+
+            /* target_name */
+            if (settings.target_name != '') {
+                $this.$target_select.attr({'name': settings.target_name, 'id': settings.target_name});
+            }
+
+            /* remaining_name */
+            if (settings.remaining_name != '') {
+                $this.$remaining_select.attr({'name': settings.remaining_name, 'id': settings.remaining_name});
+            }
+
             /* #=============================================================================== */
             /* # Wire internal events */
             /* #=============================================================================== */
-            _this.$add_btn.click(function () {
-                _this.move_elems(_this.$remaining_select.val(), false, true);
+
+            $this.$add_btn.click(function () {
+                $this.move_elems($this.$remaining_select.val(), false, true);
             });
-            _this.$remove_btn.click(function () {
-                _this.move_elems(_this.$target_select.val(), true, false);
+
+            $this.$remove_btn.click(function () {
+                $this.move_elems($this.$target_select.val(), true, false);
             });
-            _this.$choose_all_btn.click(function () {
-                _this.move_all(false, true);
+
+            $this.$choose_all_btn.click(function () {
+                $this.move_all(false, true);
             });
-            _this.$clear_all_btn.click(function () {
-                _this.move_all(true, false);
+
+            $this.$clear_all_btn.click(function () {
+                $this.move_all(true, false);
             });
-            _this.$filter_input.keyup(function () {
-                _this.update_lists(true);
+
+            $this.delegate('select.remaining > option', 'dblclick', function () {
+                $this.move_elems($this.$remaining_select.val(), false, true);
+            });
+
+            $this.delegate('select.target > option', 'dblclick', function () {
+                $this.move_elems($this.$target_select.val(), true, false);
+            });
+
+            $this.$filter_input.keyup(function () {
+                $this.update_lists(true);
             });
             /* #=============================================================================== */
             /* # Implement public functions */
             /* #=============================================================================== */
-            _this.populate = function (input) {
+
+            $this.populate = function (input) {
                 // input: [{value:_, content:_}]
-                _this.$filter_input.val('');
+                $this.$filter_input.val('');
                 for (var i in input) {
                     var e = input[i];
-                    _this._remaining_list.push([{value: e.value, content: e.content}, true]);
-                    _this._target_list.push([{value: e.value, content: e.content}, false]);
+                    $this._remaining_list.push([{value: e.value, content: e.content}, true]);
+                    $this._target_list.push([{value: e.value, content: e.content}, false]);
                 }
-                _this.update_lists(true);
+                $this.update_lists(true);
             };
 
-            _this.set_values = function (values) {
-                _this.move_elems(values, false, true);
+            $this.set_values = function (values) {
+                if (typeof values === 'undefined' || values.length === 0) {
+                    $this.move_all(true, false);
+                } else {
+                    $this.move_elems(values, false, true);
+                }
             };
 
-            _this.get_values = function () {
-                return _this.get_internal(_this.$target_select);
+            $this.get_values = function () {
+                return $this.get_internal($this.$target_select);
             };
 
             /* #=============================================================================== */
             /* # Implement private functions */
             /* #=============================================================================== */
-            _this.get_internal = function (selector) {
+            $this.get_internal = function (selector) {
                 var res = [];
                 selector.find('option').each(function () {
                     res.push($(this).val());
@@ -97,24 +125,24 @@
                 return res;
             };
 
-            _this.to_dict = function (list) {
+            $this.to_dict = function (list) {
                 var res = {};
                 for (var i in list)
                     res[list[i]] = true;
                 return res;
             };
 
-            _this.update_lists = function (force_hilite_off) {
+            $this.update_lists = function (force_hilite_off) {
                 var old;
                 if (!force_hilite_off) {
-                    old = [_this.to_dict(_this.get_internal(_this.$remaining_select)),
-                        _this.to_dict(_this.get_internal(_this.$target_select))];
+                    old = [$this.to_dict($this.get_internal($this.$remaining_select)),
+                        $this.to_dict($this.get_internal($this.$target_select))];
                 }
 
-                _this.$remaining_select.empty();
-                _this.$target_select.empty();
-                var lists = [_this._remaining_list, _this._target_list];
-                var source = [_this.$remaining_select, _this.$target_select];
+                $this.$remaining_select.empty();
+                $this.$target_select.empty();
+                var lists = [$this._remaining_list, $this._target_list];
+                var source = [$this.$remaining_select, $this.$target_select];
 
                 for (var i in lists) {
                     for (var j in lists[i]) {
@@ -129,8 +157,8 @@
                     }
                 }
 
-                _this.$remaining_select.find('option').each(function () {
-                    var inner = _this.$filter_input.val().toLowerCase();
+                $this.$remaining_select.find('option').each(function () {
+                    var inner = $this.$filter_input.val().toLowerCase();
                     var outer = $(this).html().toLowerCase();
                     if (outer.indexOf(inner) == -1) {
                         $(this).remove();
@@ -138,32 +166,33 @@
                 });
             };
 
-            _this.move_elems = function (values, b1, b2) {
+            $this.move_elems = function (values, remaining, target) {
                 for (var i in values) {
                     val = values[i];
-                    for (var j in _this._remaining_list) {
-                        var e = _this._remaining_list[j];
+                    for (var j in $this._remaining_list) {
+                        var e = $this._remaining_list[j];
                         if (e[0].value == val) {
-                            e[1] = b1;
-                            _this._target_list[j][1] = b2;
+                            e[1] = remaining;
+                            $this._target_list[j][1] = target;
                         }
                     }
                 }
-                _this.update_lists(false);
+                $this.update_lists(false);
             };
 
-            _this.move_all = function (b1, b2) {
-                for (var i in _this._remaining_list) {
-                    _this._remaining_list[i][1] = b1;
-                    _this._target_list[i][1] = b2;
+            $this.move_all = function (remaining, target) {
+                for (var i in $this._remaining_list) {
+                    $this._remaining_list[i][1] = remaining;
+                    $this._target_list[i][1] = target;
                 }
-                _this.update_lists(false);
+                $this.update_lists(false);
             };
 
-            _this.data('bootstrapTransfer', _this);
-            return _this;
+            $this.data('bootstrapTransfer', $this);
+            return $this;
         });
     };
+
     $.fn.bootstrapTransfer.defaults = {
         'template': '<div class="multi-select-transfer">\
                         <div class="mst-filters">\
@@ -175,33 +204,35 @@
                         </div>\
                         <div class="mst-selects">\
                             <div class="mst-left">\
-                                <select multiple="multiple" name="remaining" class="filtered remaining mst-origin"></select>\
+                                <select multiple="multiple" name="remaining" class="filtered remaining mst-origin form-control">\
+                                </select>\
                             </div>\
                             <div class="mst-right">\
                                 <div class="mst-buttons">\
-                                    <button type="button" class="btn btn-default mst-select-add selector-add">\
+                                    <button type="button" class="btn btn-default mst-select-add button-add">\
                                         <span class="glyphicon glyphicon-chevron-right"></span>\
                                     </button>\
-                                    <button type="button" class="btn btn-default mst-select-rem selector-remove">\
+                                    <button type="button" class="btn btn-default mst-select-rem button-remove">\
                                         <span class="glyphicon glyphicon-chevron-left"></span>\
                                     </button>\
-                                    <button type="button" class="btn btn-default mst-select-add-all selector-chooseall">\
+                                    <button type="button" class="btn btn-default mst-select-add-all button-chooseall">\
                                         <span class="glyphicon glyphicon-chevron-right"></span>\
                                         <span class="glyphicon glyphicon-chevron-right"></span>\
                                     </button>\
-                                    <button type="button" class="btn btn-default mst-select-rem-all selector-clearall">\
+                                    <button type="button" class="btn btn-default mst-select-rem-all button-clearall">\
                                         <span class="glyphicon glyphicon-chevron-left"></span>\
                                         <span class="glyphicon glyphicon-chevron-left"></span>\
                                     </button>\
                                 </div>\
                                 <div class="mst-select-destiny">\
-                                    <select multiple="multiple" name="target" class="filtered target mst-destiny"></select>\
+                                    <select multiple="multiple" name="target" class="filtered target mst-destiny form-control">\
+                                    </select>\
                                 </div>\
                             </div>\
                         </div>\
                     </div>',
-        'height': '10em',
-        'hilite_selection': true,
-        'target_id': ''
+        hilite_selection: true,
+        remaining_name: '',
+        target_name: ''
     };
 })(jQuery);
